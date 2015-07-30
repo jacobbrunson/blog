42141f6ef8fc97576db9293dc97f899242bcfa61
jacobbrunson <jacob@brunson.me>
2015-07-30 01:03:24 -0500

    finished the tutorial lol

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+	http_basic_authenticate_with name: "jacob", password: "apples",
+																 except: [:index, :show]
+
 	def index
 		@articles = Article.all
 	end
